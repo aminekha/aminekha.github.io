fdee5ac2cf391221f6fb760a720b17bb8e62f904
Amine Khaoui <aminekhaoui97@gmail.com>
2020-12-02 15:58:57 +0100

    fix zoom

--- photography/style/js/scripts.js
+++ photography/style/js/scripts.js
@@ -2,8 +2,16 @@ $(document).ready(function() {
     'use strict';
     // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
     let vh = window.innerHeight * 0.01;
+    console.log(vh);
     // Then we set the value in the --vh custom property to the root of the document
     document.documentElement.style.setProperty('--vh', `${vh}px`); 
+
+    // We listen to the resize event
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
 
 
     document.addEventListener('swiped-right', function(e) {
